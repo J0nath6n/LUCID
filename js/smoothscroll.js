2e5b43c9800a96d4bdb99beb66433693316a5f22
J0nath6n <tuflya007@gmail.com>
2021-02-28 12:33:48 +0200

    Uploaded a small change

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,9 +1,5 @@
 // SmoothScroll v0.9.9
-// Licensed under the terms of the MIT license.
-// People involved
-// - Balazs Galambosi: maintainer (CHANGELOG.txt)
-// - Patrick Brunner (patrickb1991@gmail.com)
-// - Michael Herf: ssc_pulse Algorithm
+
 
 function ssc_init() {
     if (!document.body) return;
