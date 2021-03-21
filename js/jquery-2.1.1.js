3e8f87f3ded1260b0dace1a46afb265570f51704
J0nath6n <tuflya007@gmail.com>
2021-03-21 18:06:52 +0200

    add normal map ( not error )

--- js/jquery-2.1.1.js
+++ js/jquery-2.1.1.js
@@ -4,12 +4,6 @@
  *
  * Includes Sizzle.js
  * http://sizzlejs.com/
- *
- * Copyright 2005, 2014 jQuery Foundation, Inc. and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: 2014-05-01T17:11Z
  */
 
 (function( global, factory ) {
